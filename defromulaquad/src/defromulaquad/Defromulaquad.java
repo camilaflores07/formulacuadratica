4ad3042c2f90d738a25ea2351d9f528afe1e829b
Camila Flores <camila_maria@unitec.edu>
2024-12-14 19:45:41 -0600

    Add files via upload

---
+++ defromulaquad/src/defromulaquad/Defromulaquad.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package defromulaquad;
+
+public class Defromulaquad {
+
+    
+    public static void main(String[] args) {
+        pantallita pan = new pantallita();
+        pan.setVisible(true);
+        ///dibujar pantalla 
+      
+    }
+    
+}
